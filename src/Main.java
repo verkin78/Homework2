cb7d6bf2ac37a7b852e4be43ba4c4e4e9ce87a7b
Verkin78 <otrajenie.jc@gmail.com>
2022-11-02 21:20:46 +0300

    hw2.2

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+
+
+    }
+}
